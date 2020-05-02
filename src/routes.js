d519bc7a72e60e986dc16da2561994b2ff999892
Guilherme Farias <guicfarias11@gmail.com>
2020-05-02 03:47:53 -0300

    Adding GET and POST method to the user route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserController = require('./controllers/UserController');
 const routes = express.Router();
 
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
 

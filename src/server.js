a0b565167fbaee8f8d461828532f2083cd5bea4b
Guilherme Farias <guicfarias11@gmail.com>
2020-05-02 03:40:31 -0300

    Improving routes structures

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes');
+require('./database');
 
 
 const app = express();

36b3341b81f07d4190a18ceb165cba4551859705
Guilherme Farias <guicfarias11@gmail.com>
2020-05-02 05:16:41 -0300

    Adding the address route and its user relationship

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
+
 class User extends Model{
     static init(sequelize){
         super.init({
@@ -9,7 +10,10 @@ class User extends Model{
         }, {
             sequelize
         })
-    }
+    };
+    static associate(models){
+        this.hasMany(models.Address, {foreignKey: 'user_id', as: 'addresses'})
+    };
 }
 
 module.exports = User;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const AddressesController = require('./controllers/AddressController');
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.get('/users/:user_id/addresses', AddressesController.index);
+routes.post('/users/:user_id/addresses', AddressesController.store);
 
 
 module.exports = routes;

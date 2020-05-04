823e747c55bb8e4f047acf2cd9f006a9d216f748
Guilherme Farias <guicfarias11@gmail.com>
2020-05-04 13:39:01 -0300

    Creating more complex query, for reporting

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,33 @@
+const {Op} = require('sequelize');
+const User = require('../models/User');
+
+module.exports = {
+    async show(req, res){
+        const users = await User.findAll({
+            attributes:['name', 'email'],
+            where:{
+                email: {
+                    [Op.like]: '%@gmail.com'
+                }
+            },
+            include:[
+                {
+                    association: 'addresses', 
+                    where:{
+                        street: 'Rua Touro'
+                }},//address
+                {
+                    association:'techs',
+                    //required: false,
+                    where:{
+                        name:{
+                            [Op.like]: 'Node%'
+                        }
+                    }
+                },//techs
+            ]
+        })
+        return res.json(users);
+
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserController = require('./controllers/UserController');
 const AddressesController = require('./controllers/AddressController');
 const TechsController = require('./controllers/TechController');
+const ReportController = require('./controllers/ReportController');
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
@@ -13,5 +14,10 @@ routes.post('/users/:user_id/addresses', AddressesController.store);
 routes.get('/users/:user_id/techs', TechsController.index);
 routes.post('/users/:user_id/techs', TechsController.store);
 routes.delete('/users/:user_id/techs', TechsController.delete);
+
+
+
+routes.get('/report', ReportController.show)
+
 
 module.exports = routes;
